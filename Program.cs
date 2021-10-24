09a433aeedec5f995a1a408d221329dfb2021be5
C0ppens <45225697+C0ppens@users.noreply.github.com>
2021-10-24 22:59:07 +1100

    String reverse algo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C__Algo_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StringReverser alphabetReverse = new StringReverser("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+            Console.WriteLine(alphabetReverse.reversed);
+        }
+    }
+}
---
+++ StringAlgorithm/StringReverser.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace C__Algo_s
+{
+    class StringReverser
+    {
+        private string _stringToReverse;
+        private string _reversedString;
+        public StringReverser(string s)
+        {
+            this._stringToReverse = s;
+
+            if (validateString(_stringToReverse))
+            {
+                _reversedString = reverseString(_stringToReverse);
+            }
+        }
+
+        private string reverseString(string nonReversedString)
+        {
+            String reverseString = "";
+            for (int i = nonReversedString.Length - 1; i >= 0; i--)
+            {
+                reverseString = reverseString + nonReversedString[i];
+            }
+            return reverseString;
+        }
+
+        private bool validateString(String s)
+        {
+            if (s == null || s.Length == 0)
+            {
+                throw new NullReferenceException("String is Empty or Null");
+            }
+
+            return true;
+
+        }
+
+        public string reversed
+        {
+            get => _reversedString;
+        }
+
+
+    }
+
+}
